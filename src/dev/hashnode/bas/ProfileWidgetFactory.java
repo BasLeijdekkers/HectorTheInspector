e6a8cb21662c68134e30c115b6c7e7b4a2ef44bd
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-09-22 20:59:34 +0200

    add copyright notice and license

--- src/dev/hashnode/bas/ProfileWidgetFactory.java
+++ src/dev/hashnode/bas/ProfileWidgetFactory.java
@@ -1,3 +1,4 @@
+// Copyright 2020 Bas Leijdekkers Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package dev.hashnode.bas;
 
 import com.intellij.openapi.project.Project;
